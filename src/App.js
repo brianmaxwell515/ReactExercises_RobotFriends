e0cc7e5b0bde7959cfba1967a99123519b1736b2
brianmaxwell515 <63136440+brianmaxwell515@users.noreply.github.com>
2020-06-24 15:32:48 +0800

    CSS update & connect to json response

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((user) => this.setState({ robots: user }));
   }
 
   onSearchChange = (event) => {
@@ -23,14 +29,17 @@ class App extends React.Component {
         .includes(this.state.searchfield.toLowerCase());
     });
 
-    return (
-      <div class="tc">
-        <h1>RobotFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc f1">Loading</h1>;
+    } else {
+      return (
+        <div class="tc">
+          <h1 className="f1">RobotFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
-
 export default App;
