6e9e41be5479e0bb36817f36295b2e4a4afb6c9a
brianmaxwell515 <63136440+brianmaxwell515@users.noreply.github.com>
2020-06-24 09:42:22 +0800

    Grid Done

---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SearchBox = () => {
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="Search Robots"
+        className="tc pa3 ba b--green bg-lightest-blue"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
