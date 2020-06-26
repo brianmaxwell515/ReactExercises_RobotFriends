692f46c6f7234ed84bb7fc43a8213fe7c7b9d817
brianmaxwell515 <63136440+brianmaxwell515@users.noreply.github.com>
2020-06-26 15:57:52 +0800

    js cleaning

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,25 +24,22 @@ class App extends React.Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
-    if (this.state.robots.length === 0) {
-      return <h1 className="tc f1">Loading</h1>;
-    } else {
-      return (
-        <div class="tc">
-          <h1 className="f1">RobotFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-    }
+    return !robots.length ? (
+      <h1 className="tc f1">Loading</h1>
+    ) : (
+      <div class="tc">
+        <h1 className="f1">RobotFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
   }
 }
 export default App;
