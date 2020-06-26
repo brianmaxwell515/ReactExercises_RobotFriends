45aa557836cfad2844a0e33a96e4fc595ffec885
brianmaxwell515 <63136440+brianmaxwell515@users.noreply.github.com>
2020-06-26 16:16:47 +0800

    Updates

---
+++ precache-manifest.3c240f6416e4e0fe95e96fe2b4bad6d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e357c147bd2acbc970d6acb3b94a19",
+    "url": "/ReactExercises_RobotFriends/index.html"
+  },
+  {
+    "revision": "cc0d58ead9737cd69c0e",
+    "url": "/ReactExercises_RobotFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3374265e435df5bff33c",
+    "url": "/ReactExercises_RobotFriends/static/css/main.dec08f56.chunk.css"
+  },
+  {
+    "revision": "cc0d58ead9737cd69c0e",
+    "url": "/ReactExercises_RobotFriends/static/js/2.2a06c861.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactExercises_RobotFriends/static/js/2.2a06c861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3374265e435df5bff33c",
+    "url": "/ReactExercises_RobotFriends/static/js/main.a04a0ace.chunk.js"
+  },
+  {
+    "revision": "119d6a644d0aef006a8f",
+    "url": "/ReactExercises_RobotFriends/static/js/runtime-main.1c74fb86.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ReactExercises_RobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
